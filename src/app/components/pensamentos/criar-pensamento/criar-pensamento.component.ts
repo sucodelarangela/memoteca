87322113d7e959393f7bf171406c17005d2d3fcd
Angela Caldas <aes.caldas@gmail.com>
2023-01-18 15:16:59 -0300

    feat: two-way data binding

--- src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -10,7 +10,7 @@ export class CriarPensamentoComponent {
     id: '1',
     conteudo: 'Aprendendo Angular',
     autoria: 'Angela Caldas',
-    modelo: ''
+    modelo: 'modelo1'
   };
 
   criarPensamento() {
