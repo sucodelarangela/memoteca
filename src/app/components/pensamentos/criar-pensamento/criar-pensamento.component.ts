f45f86d2e7d392dc3e5f8e0020b14de67546b209
Angela Caldas <aes.caldas@gmail.com>
2023-01-18 14:57:10 -0300

    feat: property binding and interpolation

--- src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./criar-pensamento.component.css']
 })
 export class CriarPensamentoComponent {
-
+  pensamento = {
+    id: '1',
+    conteudo: 'Aprendendo Angular',
+    autoria: 'Angela Caldas',
+    modelo: ''
+  };
 }
